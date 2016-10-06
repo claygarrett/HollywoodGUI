3aae558713fbfafc6f22b3d6ada936fd8649e011
Clay Garrett <claygarrett@gmail.com>
2016-10-05 22:29:13 -0400

    Initial commit.

---
+++ HollywoodGUI/HollywoodGUI.h
@@ -0,0 +1,19 @@
+//
+//  HollywoodGUI.h
+//  HollywoodGUI
+//
+//  Created by Clay Garrett on 10/5/16.
+//  Copyright © 2016 Clay Garrett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HollywoodGUI.
+FOUNDATION_EXPORT double HollywoodGUIVersionNumber;
+
+//! Project version string for HollywoodGUI.
+FOUNDATION_EXPORT const unsigned char HollywoodGUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HollywoodGUI/PublicHeader.h>
+
+
